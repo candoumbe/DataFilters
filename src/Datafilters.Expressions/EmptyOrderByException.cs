2452cfbefe054c6ae8f1ce6864e7dcb0dd9a4359
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2019-03-24 15:06:47 +0100

    Added new EmptyOrderByException which is thrown when calling extension method with an empty collection of OrderClause<T>

---
+++ src/Datafilters.Expressions/EmptyOrderByException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Datafilters.Expressions
+{
+    /// <summary>
+    /// Exception thrown when calling <see cref="QueryableExtensions.OrderBy{T}(IQueryable{T}, in IEnumerable{OrderClause{T}})"/> with
+    /// an empty orderBy
+    /// </summary>
+    public class EmptyOrderByException : ArgumentException
+    {
+    }
+}
