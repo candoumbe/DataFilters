1d930d2d403a6f8324c98fb83c7c0fe9b49b72e5
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2020-10-24 13:27:49 +0200

    Update Restore step to use Nuget when building from server

--- build/Build.cs
+++ build/Build.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.AzurePipelines;
@@ -11,11 +12,14 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.NuGet;
 using Nuke.Common.Utilities.Collections;
+
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.NuGet.NuGetTasks;
 
 
 [AzurePipelines(
@@ -47,13 +51,15 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
+
+    [CI] readonly AzurePipelines AzurePipelines;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "test";
@@ -73,17 +79,27 @@ class Build : NukeBuild
     Target Restore => _ => _
         .Executes(() =>
         {
-            DotNetRestore(s => s
-                .SetProjectFile(Solution)
-                .SetConfigFile(RootDirectory / "nuget.config")
-                .SetIgnoreFailedSources(true)
-                );
+            if (IsServerBuild)
+            {
+                NuGetRestore(s => s
+                    .SetTargetPath(Solution)
+                    .SetRecursive(true)
+                    .SetConfigFile(RootDirectory / "nuget.config"));
+            }
+            else
+            {
+                DotNetRestore(s => s
+                    .SetProjectFile(Solution)
+                    .SetConfigFile(RootDirectory / "nuget.config")
+                    .SetIgnoreFailedSources(true));
+            }
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+
             DotNetBuild(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
