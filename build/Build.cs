91877b7a3fa790527b7e09b2a61dd60bb6470e91
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2020-10-30 21:06:32 +0100

    Removes downloading script to install Azure Credentials Provider

--- build/Build.cs
+++ build/Build.cs
@@ -77,15 +77,15 @@ class Build : NukeBuild
 
     Target SetupNuget => _ => _
         .Before(Restore)
-        .Executes(async () =>
+        .Executes(() =>
         {
             Info("Installing Azure Credentials Provider");
-            AbsolutePath azureCredentialScript = TemporaryDirectory / "azure-credentials.ps1";
-            await HttpDownloadFileAsync("https://aka.ms/install-artifacts-credprovider.ps1", azureCredentialScript).ConfigureAwait(true);
+            //AbsolutePath azureCredentialScript = TemporaryDirectory / "azure-credentials.ps1";
+            //await HttpDownloadFileAsync("https://aka.ms/install-artifacts-credprovider.ps1", azureCredentialScript).ConfigureAwait(true);
             //if (IsWin)
             //{
-            //Powershell(arguments: @"iex "" & { $(irm https://aka.ms/install-artifacts-credprovider.ps1) }""",
-            Powershell(arguments: azureCredentialScript,
+            //Powershell(arguments: @"& ""azureCredentialScript""",
+            Powershell(arguments: @"& { $(irm https://aka.ms/install-artifacts-credprovider.ps1) }""",
                            logInvocation: true,
                            logOutput: true
                     );
@@ -96,7 +96,7 @@ class Build : NukeBuild
             //         logInvocation: true,
             //         logOutput: true);
             //}
-            DeleteFile(azureCredentialScript);
+            //DeleteFile(azureCredentialScript);
             Info("Azure Credentials Provider installed successfully");
         });
 
