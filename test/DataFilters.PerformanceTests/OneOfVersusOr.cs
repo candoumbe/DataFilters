dec0181f0564f5bac0b5e88ec25a66929e3e0fde
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2021-07-12 18:12:46 +0200

    Added a benchmark test project to start comparing various expression syntaxes on performance

---
+++ test/DataFilters.PerformanceTests/OneOfVersusOr.cs
@@ -0,0 +1,17 @@
+﻿namespace DataFilters.PerfomanceTests
+{
+    using BenchmarkDotNet.Attributes;
+
+    using System;
+    
+    public class OneOfVsOr
+    {   
+        [Benchmark]
+        [Arguments("Name=Br[a-f]")]
+        public IFilter OneOf(string input) => input.ToFilter<SuperHero>();
+
+        [Benchmark]
+        [Arguments("Name=Bra|Brb|Brc|Brd|Bre|Brf")]
+        public IFilter Or(string input) => input.ToFilter<SuperHero>();
+    }
+}
