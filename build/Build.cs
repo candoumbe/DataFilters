4adedf7f69e1fe04cc770ff78cf04edf83914c1f
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2021-11-10 23:23:01 +0100

    Switched ubuntu agents instead of windows agents

--- build/Build.cs
+++ build/Build.cs
@@ -34,7 +34,7 @@ namespace DataFilters.ContinuousIntegration
 
     [GitHubActions(
         "pull-request",
-        GitHubActionsImage.WindowsLatest, GitHubActionsImage.MacOsLatest,
+        GitHubActionsImage.UbuntuLatest, GitHubActionsImage.MacOsLatest,
         OnPullRequestBranches = new[] { DevelopBranch },
         PublishArtifacts = true,
         InvokedTargets = new[] { nameof(Tests), nameof(ReportCoverage) },
@@ -53,7 +53,7 @@ namespace DataFilters.ContinuousIntegration
     )]
     [GitHubActions(
         "integration",
-        GitHubActionsImage.WindowsLatest, GitHubActionsImage.MacOsLatest,
+        GitHubActionsImage.UbuntuLatest, GitHubActionsImage.MacOsLatest,
         OnPushBranchesIgnore = new[] { MainBranchName },
         PublishArtifacts = true,
         InvokedTargets = new[] { nameof(Tests), nameof(ReportCoverage), nameof(Pack) },
@@ -73,7 +73,7 @@ namespace DataFilters.ContinuousIntegration
     )]
     [GitHubActions(
         "delivery",
-        GitHubActionsImage.WindowsLatest, GitHubActionsImage.MacOsLatest,
+        GitHubActionsImage.UbuntuLatest, GitHubActionsImage.MacOsLatest,
         OnPushBranches = new[] { MainBranchName, ReleaseBranchPrefix + "/*" },
         InvokedTargets = new[] { nameof(ReportCoverage), nameof(Publish), nameof(AddGithubRelease) },
         ImportGitHubTokenAs = nameof(GitHubToken),
