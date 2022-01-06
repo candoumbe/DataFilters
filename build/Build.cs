9b72224e1b7f9a8f22ad96434c5c3eb15623a8c8
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2022-01-06 01:03:28 +0100

    Set `ArtifactsDirectory` as working directory for running benchmarks

--- build/Build.cs
+++ build/Build.cs
@@ -606,11 +606,12 @@ namespace DataFilters.ContinuousIntegration
                     {
                         IReadOnlyCollection<string> frameworks = csproj.GetTargetFrameworks();
                         return s.SetConfiguration(Configuration.Release)
-                                                            .SetProjectFile(csproj)
-                                                            .SetProcessArgumentConfigurator(args => args.Add("-- --filter {0}", "*", customValue: true)
-                                                                                                        .Add("--artifacts {0}", BenchmarkDirectory)
-                                                                                                        .Add("--join"))
-                                                            .CombineWith(frameworks, (setting, framework) => setting.SetFramework(framework));
+                                .SetProjectFile(csproj)
+                                .SetProcessWorkingDirectory(ArtifactsDirectory)
+                                .SetProcessArgumentConfigurator(args => args.Add("-- --filter {0}", "*", customValue: true)
+                                                                            .Add("--artifacts {0}", BenchmarkDirectory)
+                                                                            .Add("--join"))
+                                .CombineWith(frameworks, (setting, framework) => setting.SetFramework(framework));
                     });
                 });
             });
