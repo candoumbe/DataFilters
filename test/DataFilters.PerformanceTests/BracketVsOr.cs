c132a57888895e499afe075eed5a7a17edb9ec6f
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2024-09-21 21:22:48 +0200

    tests: add performance tests for `Group` parser

--- test/DataFilters.PerformanceTests/BracketVsOr.cs
+++ test/DataFilters.PerformanceTests/BracketVsOr.cs
@@ -1,26 +1,26 @@
-﻿namespace DataFilters.PerfomanceTests
+﻿namespace DataFilters.PerformanceTests
 {
     using System;
     using BenchmarkDotNet.Attributes;
     using BenchmarkDotNet.Jobs;
 
-    [MemoryDiagnoser]
-    [SimpleJob(RuntimeMoniker.CoreRt31)]
-    [SimpleJob(RuntimeMoniker.Net50)]
-    [SimpleJob(RuntimeMoniker.Net60)]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Marquer les membres comme étant static")]
-    public class BracketVsOr
-    {
-        [Benchmark]
-        [Arguments("Nickname=Br[a-f]")]
-        public IFilter Bracket(string input) => input.ToFilter<SuperHero>();
-
-        [Benchmark]
-        [Arguments("Nickname=(Bra|Brb)|(Brc|Brd)|(Bre|Brf)")]
-        public IFilter Or(string input) => input.ToFilter<SuperHero>();
-
-        [Benchmark]
-        [Arguments("Nickname={Bra|Brb|Brc|Brd|Bre|Brf}")]
-        public IFilter OneOf(string input) => input.ToFilter<SuperHero>();
-    }
+    // [MemoryDiagnoser]
+    // [SimpleJob(RuntimeMoniker.CoreRt31)]
+    // [SimpleJob(RuntimeMoniker.Net50)]
+    // [SimpleJob(RuntimeMoniker.Net60)]
+    // [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Marquer les membres comme étant static")]
+    // public class BracketVsOr
+    // {
+    //     [Benchmark]
+    //     [Arguments("Nickname=Br[a-f]")]
+    //     public IFilter Bracket(string input) => input.ToFilter<SuperHero>();
+    //
+    //     [Benchmark]
+    //     [Arguments("Nickname=(Bra|Brb)|(Brc|Brd)|(Bre|Brf)")]
+    //     public IFilter Or(string input) => input.ToFilter<SuperHero>();
+    //
+    //     [Benchmark]
+    //     [Arguments("Nickname={Bra|Brb|Brc|Brd|Bre|Brf}")]
+    //     public IFilter OneOf(string input) => input.ToFilter<SuperHero>();
+    // }
 }
---
+++ test/DataFilters.PerformanceTests/GroupParserPerformanceTests.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using DataFilters.Grammar.Parsing;
+using Superpower;
+using Superpower.Model;
+
+namespace DataFilters.PerformanceTests
+{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net60)]
+    [SimpleJob(RuntimeMoniker.Net80)]
+    [SuppressMessage("Performance", "CA1822:Marquer les membres comme étant static")]
+    public class GroupParserPerformanceTests
+    {
+        [Params(1, 2, 3, 5)] // Paramètres pour le nombre de parenthèses
+        public int ParenthesesCount;
+
+        private string _constructedString;
+        private const string FixedText = "12:34:56.789";
+        private FilterTokenizer _tokenizer;
+        private TokenList<FilterToken> _tokens;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _tokenizer = new FilterTokenizer();
+            StringBuilder sb = new StringBuilder();
+            
+            // Ajout des parenthèses ouvrantes
+            for (int i = 0; i < ParenthesesCount; i++)
+            {
+                sb.Append('(');
+            }
+
+            // Ajout du texte fixe
+            sb.Append(FixedText);
+
+            // Ajout des parenthèses fermantes
+            for (int i = 0; i < ParenthesesCount; i++)
+            {
+                sb.Append(')');
+            }
+
+            _constructedString = sb.ToString();
+            
+            _tokens = _tokenizer.Tokenize(_constructedString);
+        }
+        
+        [Benchmark]
+        public void Parse() => FilterTokenParser.Group.Parse(_tokens);
+    
+    }
+}
